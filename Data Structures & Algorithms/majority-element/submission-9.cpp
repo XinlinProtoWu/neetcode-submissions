6e9356cfe41bc24ae8e1d40dc2a4a6a88fc3d3ff
XinlinProtoWu <xinlinwu0116@gmail.com>
2026-08-02 21:55:01 -0400

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        while (true) {
+            int candidate = nums[rand() % (nums.size())];
+            int count = 0;
+            for (int num : nums) {
+                if (num == candidate) {
+                    count++;
+                }
+            }
+            if (count > nums.size() / 2) {
+                return candidate;
+            }
+        }
+    }
+};
